ac1ab84a2974ff9c5a1f3e93e5f8e34b866ab348
Chris Johnson (Oracle) <christopher.johnson@oracle.com>
2020-09-13 23:03:22 -0400

    Implement DELETE
    Note: this requires a new route in the API GW for "/dreg/{uri*}" that points to the same Function.

--- dynamicreg/IDCSClient.py
+++ dynamicreg/IDCSClient.py
@@ -43,23 +43,23 @@ class IDCSClient:
         self.accessToken = token.get("access_token")
         return
 
-    def GetUsers(self):
-        logging.debug("GetUsers() called")
-        response = self.oauthClient.get(self.idcsUrl + "/admin/v1/Users")
-        print("Status code: {}".format(response.status_code))
-        if response.ok:
-            print( "Response indicates success" )
-        else:
-            print( "Error!" )
+    # def GetUsers(self):
+    #     logging.debug("GetUsers() called")
+    #     response = self.oauthClient.get(self.idcsUrl + "/admin/v1/Users")
+    #     print("Status code: {}".format(response.status_code))
+    #     if response.ok:
+    #         print( "Response indicates success" )
+    #     else:
+    #         print( "Error!" )
 
-    def GetApps(self):
-        logging.debug("GetApps() called")
-        response = self.oauthClient.get(self.idcsUrl + "/admin/v1/Apps")
-        print("Status code: {}".format(response.status_code))
-        if response.ok:
-            print( "Response indicates success" )
-        else:
-            print( "Error!" )
+    # def GetApps(self):
+    #     logging.debug("GetApps() called")
+    #     response = self.oauthClient.get(self.idcsUrl + "/admin/v1/Apps")
+    #     print("Status code: {}".format(response.status_code))
+    #     if response.ok:
+    #         print( "Response indicates success" )
+    #     else:
+    #         print( "Error!" )
 
     def CreateApp(self, clientName, redirectUris):
         logging.debug("CreateApp() called")
@@ -91,13 +91,42 @@ class IDCSClient:
             logging.debug("ID not present in response!")
             raise Exception("Failed to get ID for newly created app!" )
 
-        # we need client ID, secret
         logging.debug("Activating newly created app with id {}".format(id))
-        appActivatePayload = {"active": True, "schemas": ["urn:ietf:params:scim:schemas:oracle:idcs:AppStatusChanger"]}
-        activateResponse = self._sendRequest( "PUT", "/admin/v1/AppStatusChanger/" + id, appActivatePayload )
+        self.SetAppActiveStatus( id, True)
 
+        # The caller needs the client ID + secret
         logging.debug("Returning client ID + client secret")
         return (createResponse.get("name"), createResponse.get("clientSecret"))
+
+    def SetAppActiveStatus(self, id, status):
+        appActivatePayload = {"active": status, "schemas": ["urn:ietf:params:scim:schemas:oracle:idcs:AppStatusChanger"]}
+        activateResponse = self._sendRequest( "PUT", "/admin/v1/AppStatusChanger/" + id, appActivatePayload )
+
+    def DeleteApp(self, id):
+        logging.debug("Deleting app with ID {}".format(id))
+        # in order to delete an app you need to be sure it's deactivated
+        self.SetAppActiveStatus(id,False)
+        self._sendRequest( "DELETE", "/admin/v1/Apps/" + id, None)
+        return
+
+    def DeleteAppWithClientID(self, clientID):
+        # IDCS will not allow more than one app to have the same "name"
+        # so this will return either 0 or 1 results.
+        response = self._sendRequest("GET",
+                                     "/admin/v1/Apps?filter=name+eq+%22" + clientID + "%22",
+                                     None)
+
+        if response and 1 == response.get("totalResults"):
+            #response.get("name") and response.get("id"):
+            #return self.DeleteApp(response.get("id"))
+            id = response.get("Resources")[0].get("id")
+            logging.debug( "Found app to delete - IDCS id is {}".format(id))
+            self.DeleteApp(id)
+        else:
+            logging.error("Could not find app to delete!")
+            raise Exception("Unable to find app to delete")
+
+        return
 
     def _sendRequest(self, verb, uri, jsonpayload):
         logging.debug("Sending POST payload:")
@@ -112,14 +141,23 @@ class IDCSClient:
                                      })
 
         logging.debug("Status code: {}".format(response.status_code))
-        logging.debug(response.headers)
-        logging.debug(response.text)
+        # logging.debug(response.headers)
+        # logging.debug(response.text)
         if response.ok:
             logging.debug( "Response indicates success" )
-            logging.debug(response.content)
-            logging.debug(json.dumps(response.json()))
-            return response.json()
+            if response.content:
+                logging.debug(response.content)
+                if response.text:
+                    logging.debug(json.dumps(response.json()))
+                    return response.json()
+            else:
+                return None
         else:
             # anything other than "OK" from IDCS means error
             logging.error("Error making HTTP request")
+            if response.text:
+                logging.debug(response.text)
+            else:
+                logging.debug("No content to log")
+
             raise Exception( "HTTP request failed" )
--- dynamicreg/func.py
+++ dynamicreg/func.py
@@ -6,6 +6,8 @@ import time
 from fdk import response
 from IDCSClient import IDCSClient
 
+#TODO: make the IDCSClient a global variable and initialize it only once
+
 def handler(ctx, data: io.BytesIO=None):
     logging.getLogger().info("Inside handler")
 
@@ -20,51 +22,59 @@ def handler(ctx, data: io.BytesIO=None):
     statuscode = 500
     responseBody = {}
 
-    # logging.getLogger().info( json.dumps(dict(ctx.Config())))
+    if ctx.Method() == "POST":
 
-    try:
-        logging.debug("Parsing post payload: '{}'".format(data.getvalue()))
-        requestBody = json.loads(data.getvalue())
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-        raise
+        try:
+            logging.debug("Parsing post payload: '{}'".format(data.getvalue()))
+            requestBody = json.loads(data.getvalue())
+        except (Exception, ValueError) as ex:
+            logging.getLogger().info('error parsing json payload: ' + str(ex))
+            raise
 
-    # the only things I get from the Dynamic Client Request body are:
-    # 1: token_endpoint_auth_method (which had better be client_secret_basic)
-    # 2: client_name
-    # 3: redirect_uris
-    ram             = requestBody.get("token_endpoint_auth_method")
-    clientName      = requestBody.get("client_name")
-    redirect_uris   = requestBody.get("redirect_uris")
+        # the only things I get from the Dynamic Client Request body are:
+        # 1: token_endpoint_auth_method (which had better be client_secret_basic)
+        # 2: client_name
+        # 3: redirect_uris
+        ram             = requestBody.get("token_endpoint_auth_method")
+        clientName      = requestBody.get("client_name")
+        redirect_uris   = requestBody.get("redirect_uris")
 
-    if ram != "client_secret_basic":
-        # CMJ:
-        # per the DCR spec we actually should ignore this input and create the client
-        # as we want and return it back to the caller for them to deal with.
-        # I'm ignoring that because I don't want to pollute IDCS with lots of bad clients
-        raise Exception( "Only client_basic_secret is supported for token_endpoint_auth_method" )
-    else:
-        logging.debug("Creating app with the following params:")
-        logging.debug("Client Name: {}".format(clientName))
-        logging.debug("Redirect URLs: {}".format(json.dumps(redirect_uris)))
-        (id, secret) = client.CreateApp(clientName, redirect_uris)
+        if ram != "client_secret_basic":
+            # CMJ:
+            # per the DCR spec we actually should ignore this input and create the client
+            # as we want and return it back to the caller for them to deal with.
+            # I'm ignoring that because I don't want to pollute IDCS with lots of bad clients
+            raise Exception( "Only client_basic_secret is supported for token_endpoint_auth_method" )
+        else:
+            logging.debug("Creating app with the following params:")
+            logging.debug("Client Name: {}".format(clientName))
+            logging.debug("Redirect URLs: {}".format(json.dumps(redirect_uris)))
+            (id, secret) = client.CreateApp(clientName, redirect_uris)
 
-        print("Client ID     : {}".format(id))
-        print("Client secret : {}".format(secret))
+            print("Client ID     : {}".format(id))
+            print("Client secret : {}".format(secret))
 
-        responseBody = {
-            "client_id": id,
-            "client_secret": secret,
-            "client_id_issued_at": int(time.time()),
-            "client_secret_expires_at": 0,
+            responseBody = {
+                "client_id": id,
+                "client_secret": secret,
+                "client_id_issued_at": int(time.time()),
+                "client_secret_expires_at": 0,
 
-            "client_name": clientName,
-            "redirect_uris": redirect_uris,
-            "grant_types": ["authorization_code"],
-            "token_endpoint_auth_method": "client_secret_basic"
-        }
+                "client_name": clientName,
+                "redirect_uris": redirect_uris,
+                "grant_types": ["authorization_code"],
+                "token_endpoint_auth_method": "client_secret_basic"
+            }
 
+            statuscode = 200
+
+    elif ctx.Method() == "DELETE":
+        logging.debug("DELETE request received")
+        # get the last part of the URI
+        clientID = ctx.RequestURL().split("/")[-1]
+        client.DeleteAppWithClientID(clientID)
         statuscode = 200
+        responseBody = {"result": "success"}
 
     return response.Response(
         ctx, status_code=statuscode,
